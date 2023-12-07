813573d2ea6a66714f2239ef4143b27b8a495099
lucleon <luc0494@gmail.com>
2023-12-07 16:09:31 +0100

    modified lualine config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,13 +20,35 @@ return {
       lsp.preset('recommend')
 
       local cmp = require('cmp')
-      local cmp_select = {behavior = cmp.SelectBehavior.Select}
-      local cmp_mapping = lsp.defaults.cmp_mappings({
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm( {select = true}),
-        ["<C-Space>"] = cmp.mapping.complete(),
+      local cmp_action = require('lsp-zero').cmp_action()
+
+      cmp.setup({
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        }),
+        sources = {
+          {name = 'nvim_lsp'},
+        }
       })
+
+      --local cmp_mappings = lsp.defaults.cmp_mappings({
+      --  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+      --  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+      --  ['<C-y>'] = cmp.mapping.confirm( {select = true}),
+      --  ["<C-Space>"] = cmp.mapping.complete(),
+      --})
+
+      --lsp.setup_nvim_cmp({
+      --  mapping = cmp_mappings
+      --})
 
       lsp.on_attach(function(client, bufnr)
         local opts = {buffer = bufnr, remap = false}
@@ -49,7 +71,7 @@ return {
                 }
               }
             })
-          end, 
+          end,
           clangd = function()
             require('lspconfig').clangd.setup({
               settings = {
@@ -58,7 +80,7 @@ return {
                 }
               }
             })
-          end, 
+          end,
         },
       })
     end },
