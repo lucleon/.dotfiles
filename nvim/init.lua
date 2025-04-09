998160290a16f0f60b346f4e0849601547232082
lucleon <luc0494@gmail.com>
2025-04-09 18:12:18 +0200

    cleaned up dotfiles

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,6 @@ vim.g.mapleader = ','
 _G.global = {}
 _G.global.float_border_opts = { border = 'rounded', focusable = false, scope = 'line' }
 
-require('custom')
-require("lazy").setup("plugins")
+require('config')
+require("lazy")
 require("remap")
